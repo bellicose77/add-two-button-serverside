52630f50b6b12269ee368e6c41f866f071f61c73
bellicose77 <64400354+bellicose77@users.noreply.github.com>
2021-11-28 02:02:46 +0600

    done

--- index.js
+++ index.js
@@ -2,9 +2,10 @@ const express = require('express');
 const cors = require('cors');
 const app = express();
 const { MongoClient } = require('mongodb');
+require('dotenv').config()
 // const ObjectId = require('mongodb').ObjectID,
 const ObjectId = require('mongodb').ObjectId;
-const port = 5000;
+const port = process.env.PORT || 5000;
 
 //addbutton
 //RSSi2zzpWh0FENsW
@@ -13,7 +14,7 @@ const port = 5000;
 app.use(cors());
 app.use(express.json())
 
-const uri = "mongodb+srv://addbutton:RSSi2zzpWh0FENsW@cluster0.lt5tb.mongodb.net/myFirstDatabase?retryWrites=true&w=majority";
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.lt5tb.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
 
 async function run() {
